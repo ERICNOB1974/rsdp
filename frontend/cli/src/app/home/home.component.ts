c710937abce03ef23b8d778c3e7071fced4942a4
ericnob1974 <ericnob1974@gmail.com>
2024-08-30 18:11:41 -0300

    Primer commit

--- frontend/cli/src/app/home/home.component.ts
+++ frontend/cli/src/app/home/home.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4">Brifal S.A.</h1>
-      <p class="lead">Control y gestión de los partes de mano de obra.</p>
+      <h1 class="display-4">Red social S.A.</h1>
+      <p class="lead">Control y gestión de los partes de redes de social.</p>
     </div>
   `,
   styles: ``
