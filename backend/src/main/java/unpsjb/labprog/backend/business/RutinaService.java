ddce111169ae5e9859bb80d5946f93287c486c0b
ericnob1974 <ericnob1974@gmail.com>
2024-09-11 20:51:04 -0300

    Añadi clases

--- backend/src/main/java/unpsjb/labprog/backend/business/RutinaService.java
+++ backend/src/main/java/unpsjb/labprog/backend/business/RutinaService.java
@@ -1,12 +1,31 @@
 package unpsjb.labprog.backend.business;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import unpsjb.labprog.backend.model.Rutina;
 
 @Service
 public class RutinaService {
 
     @Autowired
     RutinaRepository rutinaRepository;
+
+    public List<Rutina> findAll() {
+        return rutinaRepository.findAll();
+    }
+
+    @Transactional
+    public Rutina save(Rutina rutina) {
+        return rutinaRepository.save(rutina);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        rutinaRepository.deleteById(id);
+    }
 
 }
