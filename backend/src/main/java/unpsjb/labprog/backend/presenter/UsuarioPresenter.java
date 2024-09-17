910b8d1d416dc0d769c6ace3f4dd1571d179c224
Lucas <lucassanmartin76@gmail.com>
2024-09-17 00:26:39 -0300

    amigos de amigos realizado

--- backend/src/main/java/unpsjb/labprog/backend/presenter/UsuarioPresenter.java
+++ backend/src/main/java/unpsjb/labprog/backend/presenter/UsuarioPresenter.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import unpsjb.labprog.backend.Response;
 import unpsjb.labprog.backend.business.UsuarioService;
 import unpsjb.labprog.backend.model.Usuario;
+import java.util.List;
 
 @RestController
 @RequestMapping("usuarios")
@@ -19,6 +20,20 @@ public class UsuarioPresenter {
     public ResponseEntity<Object> findAll(){
         return Response.ok(usuarioService.findAll());
     }
+
+    @GetMapping("/amigos/{nombreUsuario}")
+    public ResponseEntity<Object> obtenerAmigos(@PathVariable String nombreUsuario) {
+        List<Usuario> amigosDeAmigos = usuarioService.amigos(nombreUsuario);
+        return Response.ok(amigosDeAmigos);
+    }
+
+    @GetMapping("/amigosDeAmigos/{nombreUsuario}")
+    public ResponseEntity<Object> obtenerAmigosDeAmigos(@PathVariable String nombreUsuario) {
+        List<Usuario> amigosDeAmigos = usuarioService.amigosDeAmigos(nombreUsuario);
+        return Response.ok(amigosDeAmigos);
+    }
+
+
 
     @RequestMapping(path = "/create", method = RequestMethod.POST)
     public ResponseEntity<Object> create(@RequestBody Usuario usuario){
