d90666aaa67b1b190b6cee5467b576a5086c33d3
Lucas <lucassanmartin76@gmail.com>
2024-09-29 04:32:29 -0300

    tarjetas de comunidades roles de comunidades

--- backend/src/main/java/unpsjb/labprog/backend/presenter/ComunidadPresenter.java
+++ backend/src/main/java/unpsjb/labprog/backend/presenter/ComunidadPresenter.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import unpsjb.labprog.backend.Response;
 import unpsjb.labprog.backend.business.ComunidadService;
+import unpsjb.labprog.backend.business.UsuarioComunidadService;
 import unpsjb.labprog.backend.model.Comunidad;
 
 @RestController
@@ -16,6 +17,9 @@ public class ComunidadPresenter {
 
     @Autowired
     ComunidadService comunidadService;
+
+    @Autowired
+    private UsuarioComunidadService usuarioComunidadService;
 
     @GetMapping("/findAll")
     public ResponseEntity<Object> findAll(){
@@ -38,4 +42,43 @@ public class ComunidadPresenter {
         return Response.ok(comunidadService.findById(id));
     }
 
+    @PostMapping("/otorgarRolAdministrador/{idCreador}/{idMiembro}/{idComunidad}")
+    public ResponseEntity<Object> otorgarRolAdministrador(@PathVariable Long idCreador, @PathVariable Long idMiembro, @PathVariable Long idComunidad) {
+        try {
+            String respuesta=usuarioComunidadService.otorgarRolAdministrador(idCreador, idMiembro, idComunidad);
+            return Response.ok(respuesta);
+        } catch (Exception e) {
+            return Response.error("", "Error al otorgar rol: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/quitarRolAdministrador/{idCreador}/{idMiembro}/{idComunidad}")
+    public ResponseEntity<Object> quitarRolAdministrador(@PathVariable Long idCreador, @PathVariable Long idMiembro, @PathVariable Long idComunidad) {
+        try {
+            String respuesta=usuarioComunidadService.quitarRolAdministrador(idCreador, idMiembro, idComunidad);
+            return Response.ok(respuesta);
+        } catch (Exception e) {
+            return Response.error("", "Error al quitar rol: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/gestionarSolicitudIngreso/{idSuperUsuario}/{idMiembro}/{idComunidad}")
+    public ResponseEntity<Object> gestionarSolicitudIngreso(@PathVariable Long idSuperUsuario, @PathVariable Long idMiembro, @PathVariable Long idComunidad, @RequestParam boolean aceptada) {
+        try {
+            String respuesta=usuarioComunidadService.gestionarSolicitudes(idSuperUsuario, idMiembro, idComunidad, aceptada);
+            return Response.ok(respuesta);
+        } catch (Exception e) {
+            return Response.error("", "Error al gestionar solicitud de ingreso: " + e.getMessage());
+        }
+    }
+
+    @RequestMapping(path = "/create/{idUsuario}", method = RequestMethod.POST)
+    public ResponseEntity<Object> create(@RequestBody Comunidad comunidad,@PathVariable Long idUsuario){
+        try {
+            System.out.println("FACUUUUUUUUUUUUUUUUComunidad recibida: " + comunidad);
+            return Response.ok(usuarioComunidadService.guardarComunidadYCreador(comunidad,idUsuario));
+        } catch (Exception e) {
+            return Response.error("", "Error al crear la comunidad: " + e.getMessage()+e);
+        }
+    }
 }
