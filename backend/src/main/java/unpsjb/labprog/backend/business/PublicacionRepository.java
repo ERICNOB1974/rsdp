8839bf01d8c8cb1ccebd0aaeb397f4a2300271e1
Facundo <facuespaniol@gmail.com>
2024-10-16 19:48:49 -0300

    que hice

--- backend/src/main/java/unpsjb/labprog/backend/business/PublicacionRepository.java
+++ backend/src/main/java/unpsjb/labprog/backend/business/PublicacionRepository.java
@@ -2,15 +2,23 @@ package unpsjb.labprog.backend.business;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import unpsjb.labprog.backend.model.Publicacion;
-import unpsjb.labprog.backend.model.Usuario;
 
 @Repository
 public interface PublicacionRepository extends Neo4jRepository<Publicacion, Long> {
 
-    @Query("MATCH (u:Usuario{id:$usuario.id}), (p:Publicacion{id:$publicacion.id}) " +
-            " CREATE (u)-[:POSTEO]->(p)")
-    void establecerCreador(Publicacion publicacion, Usuario usuario);
+    @Query("MATCH (u:Usuario) WHERE id(u)=$ usuarioId " +
+            "WITH u " +
+            "MATCH (p:Publicacion) WHERE id(p)=$publicacionId " +
+            "CREATE (u)-[:POSTEO]->(p)")
+    void establecerCreador2(Long usuarioId, Long publicacionId);
+
+    @Query("MATCH (u:Usuario), (p:Publicacion) " +
+            "WHERE id(u) = $usuarioId AND id(p) = $publicacionId " +
+            "CREATE (u)-[:POSTEO]->(p)")
+    void establecerCreador(@Param("usuarioId") Long usuarioId, @Param("publicacionId") Long publicacionId);
+
 }
