3ca6b6c63b920bd71921d3c340bbb9f442e976f0
ericnob1974 <ericnob1974@gmail.com>
2024-11-02 02:54:05 -0300

    Merge

--- backend/src/main/java/unpsjb/labprog/backend/business/RutinaService.java
+++ backend/src/main/java/unpsjb/labprog/backend/business/RutinaService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import unpsjb.labprog.backend.model.Comunidad;
 import unpsjb.labprog.backend.model.Dia;
 import unpsjb.labprog.backend.model.Ejercicio;
 import unpsjb.labprog.backend.model.Etiqueta;
