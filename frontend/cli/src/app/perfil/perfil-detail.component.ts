0dbe082eb769412154bb4cc0e2b7c9d4a204f98e
ericnob1974 <ericnob1974@gmail.com>
2024-11-03 07:18:46 -0300

    Push

--- frontend/cli/src/app/perfil/perfil-detail.component.ts
+++ frontend/cli/src/app/perfil/perfil-detail.component.ts
@@ -28,6 +28,7 @@ export class PerfilDetailComponent implements OnInit {
   campoModificado: boolean = false;
   nombreUsuarioValido: boolean = true;
   nombreRealValido: boolean = true;
+  
 
   constructor(
     private usuarioService: UsuarioService,
