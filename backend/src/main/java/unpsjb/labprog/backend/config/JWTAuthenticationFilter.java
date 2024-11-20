d9c0f7e51126f33074912af9742e89c316f0f793
ericnob1974 <ericnob1974@gmail.com>
2024-11-20 14:26:05 -0300

    No se

--- backend/src/main/java/unpsjb/labprog/backend/config/JWTAuthenticationFilter.java
+++ backend/src/main/java/unpsjb/labprog/backend/config/JWTAuthenticationFilter.java
@@ -38,6 +38,7 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
         } else {
             System.out.println("Token inválido o ausente");
         }
+        
         filterChain.doFilter(request, response);
     }
 
