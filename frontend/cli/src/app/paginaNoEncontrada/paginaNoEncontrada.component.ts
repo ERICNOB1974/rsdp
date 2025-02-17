ef3c1d0efd7d10880d32e589bd6e8b59369ab42a
ericnob1974 <ericnob1974@gmail.com>
2025-02-17 03:37:55 -0300

    Pantalla de URL INEXISTENTE

---
+++ frontend/cli/src/app/paginaNoEncontrada/paginaNoEncontrada.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-error404',
+  templateUrl: './paginaNoEncontrada.component.html',
+  styleUrl: './paginaNoEncontrada.component.css'
+})
+export class PaginaNoEncontrada {
+  constructor(private router: Router) {}
+
+  volverAlInicio(): void {
+    this.router.navigate(['']); // Navega a la ruta raíz
+  }
+}
